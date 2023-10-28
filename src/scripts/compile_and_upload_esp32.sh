b429590dc600106f25a10f8da2d939635f68f9f0
peter-ranmarine <peter@ranmarine.io>
2023-10-28 19:43:07 +0200

    adding Rust code

--- src/scripts/compile_and_upload_esp32.sh
+++ src/scripts/compile_and_upload_esp32.sh
@@ -5,7 +5,16 @@ if ! ./find_usb.sh; then
     exit 1
 fi
 
+echo "Compiling for ESP32"
 arduino-cli compile \
             -b esp32:esp32:esp32 \
             -o /home/pi/build/piranha.bin \
             /home/pi/src/piranha/piranha.ino
+
+# upload binary to /dev/esp32
+echo "Uploading to /dev/esp32"
+arduino-cli upload \
+            -p /dev/esp32 \
+            -b esp32:esp32:esp32 \
+            -i /home/pi/build/piranha.bin \
+            -v
