bad61e21b534fd03a6fb90544dc0726fa2ccf43d
peter-ranmarine <peter@ranmarine.io>
2023-10-29 12:04:40 +0100

    arguments / filename

--- src/scripts/compile_and_upload_esp32.sh
+++ src/scripts/compile_and_upload_esp32.sh
@@ -8,7 +8,7 @@ fi
 echo "Compiling for ESP32"
 arduino-cli compile \
             -b esp32:esp32:esp32 \
-            -o /home/pi/build/piranha.bin \
+            --output-dir /home/pi/build/ \
             /home/pi/src/piranha/piranha.ino
 
 # upload binary to /dev/esp32
@@ -16,5 +16,5 @@ echo "Uploading to /dev/esp32"
 arduino-cli upload \
             -p /dev/esp32 \
             -b esp32:esp32:esp32 \
-            -i /home/pi/build/piranha.bin \
+            -i /home/pi/build/piranha.ino.bin \
             -v
