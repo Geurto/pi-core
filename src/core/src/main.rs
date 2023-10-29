ba441b7b1f67b60d70ce7371690131aeac9f2b39
peter-ranmarine <peter@ranmarine.io>
2023-10-29 13:17:04 +0100

    Rust code compiles?

--- src/core/src/main.rs
+++ src/core/src/main.rs
@@ -1,3 +1,20 @@
-fn main() {
-    println!("Hello, world!");
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let port_name = "/dev/esp32";
+    let mut port = serialport::new(port_name, 115_200)
+        .timeout(std::time::Duration::from_millis(10))
+        .open()
+        .expect("Failed to open port");
+
+    let mut serial_buf: Vec<u8> = vec![0; 100];
+
+    loop {
+        match port.read(serial_buf.as_mut_slice()) {
+            Ok(t) => {
+                let s = String::from_utf8_lossy(&serial_buf[..t]);
+                println!("{}", s);
+            }
+            Err(ref e) if e.kind() == std::io::ErrorKind::TimedOut => (),
+            Err(e) => eprintln!("{:?}", e),
+        }
+    }
 }
